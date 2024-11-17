e9c3c4a4e4b0bda955b9e427a1e9c39d2b69f914
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-11-17 13:17:18 +0400

    create HomeScreen & ViewModel

---
+++ app/src/main/java/com/example/movies_app_jc/presentation/screens/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.movies_app_jc.presentation.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.movies_app_jc.presentation.ui.theme.Movies_App_JCTheme
+import com.example.movies_app_jc.presentation.viewmodel.MovieListViewModel
+import com.example.movies_app_jc.util.Screen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Movies_App_JCTheme {
+
+                SetBarColor(MaterialTheme.colorScheme.inverseOnSurface)
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.rout
+                    ) {
+                        composable(Screen.Home.rout) {
+                            HomeScreen(navController)
+                        }
+
+                        composable(
+                            Screen.Details.rout + "/{movieId}",
+                            arguments = listOf(
+                                navArgument("movieId") {type = NavType.IntType}
+                            )
+                        ) { backStackEntry ->
+//                            DetailScreen(backStackEntry)
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    @Composable
+    private fun SetBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        LaunchedEffect(key1 = color) {
+            systemUiController.setSystemBarsColor(color)
+        }
+    }
+}
